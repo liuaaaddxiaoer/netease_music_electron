fa4ca58459173a76239e87ff8e1204defe207419
liuxiaoer@offcn.com <oneenddi@163.com>
2020-01-02 21:54:55 +0800

    播放暂停功能

--- src/renderer/components/HomePlayerTool.vue
+++ src/renderer/components/HomePlayerTool.vue
@@ -2,14 +2,15 @@
   <div class="container">
     <div class="player_container"></div>
     <div class="play_pause_container">
-      <i class="iconfont icon-bofang"></i>
-      <i class="iconfont icon-bofang"></i>
-      <i class="iconfont icon-bofang"></i>
-      <i class="iconfont icon-bofang"></i>
-      <i class="iconfont icon-bofang"></i>
+      <i class="like size"></i>
+      <i class="prev size"></i>
+      <i :class="play" @click="togglePlayer"></i>
+      <i class="next size"></i>
+      <i class="iconfont icon-fenxiang-"></i>
+      <div class="ok"></div>
     </div>
     <div class="volume_container">
-      <audio :src="currentURL" autoplay controls></audio>
+      <audio :src="currentURL" autoplay controls ref="audio"></audio>
     </div>
   </div>
 </template>
@@ -19,10 +20,26 @@ import { mapState } from "vuex";
 export default {
   name: "HomePlayerTool",
   data() {
-    return {};
+    return {
+      play: "pause"
+    };
   },
   computed: {
     ...mapState(["currentURL"])
+  },
+  methods: {
+    // 播放暂停
+    togglePlayer() {
+      let player = this.$refs.audio;
+      if (player.paused) {
+        player.play();
+        this.play = "pause";
+      } else {
+        this.play = "play";
+
+        player.pause();
+      }
+    }
   }
 };
 </script>
@@ -45,13 +62,54 @@ export default {
   }
   .play_pause_container {
     flex: 1;
-    background: red;
     display: flex;
     justify-content: center;
     align-items: center;
+
     i {
       font-size: 25px;
-      margin-right: 20px;
+      margin-right: 25px;
+    }
+
+    i.like {
+      background: url("../assets/images/MiniPlayerDislikeButton.png") no-repeat
+        center center;
+      width: 22px;
+      height: 22px;
+      margin-right: 40px;
+    }
+
+    .prev {
+      background: url("../assets/images/MiniPlayerRewindButton.png") no-repeat
+        center center;
+    }
+
+    .next {
+      background: url("../assets/images/MiniPlayerFastForwardButton.png")
+        no-repeat center center;
+      margin-right: 40px;
+    }
+
+    .size {
+      background-size: cover;
+      width: 16px;
+      height: 18px;
+    }
+
+    .play {
+      background: url("../assets/images/MiniPlayerPlayButton.png"),
+        no-repeat center center;
+      background-size: cover;
+      width: 45px;
+      height: 45px;
+    }
+
+    .pause {
+      background: url("../assets/images/MiniPlayerPauseButton.png"),
+        no-repeat center center;
+      background-size: cover;
+      width: 45px;
+      height: 45px;
     }
   }
 }
